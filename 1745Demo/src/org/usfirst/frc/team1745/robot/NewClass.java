f632ec6980187991ccc968b0682aa968a24a56fe
dg331474 <dg331474@PHSE105AT418129.risd.net>
2016-01-19 19:10:04 -0600

    Demo Push Day 1

---
+++ 1745Demo/src/org/usfirst/frc/team1745/robot/NewClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team1745.robot;
+
+/**
+ * @author dg331474
+ *
+ */
+public class NewClass {
+
+	/**
+	 * 
+	 */
+	public NewClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
