fcb88eef14d16026c774ef80de766acbe5802145
robots <robots@PHSE105AT418129.risd.net>
2016-02-11 16:37:06 -0600

    test effective

--- ShootingRobot/src/org/usfirst/frc/team1745/robot/Robot.java
+++ ShootingRobot/src/org/usfirst/frc/team1745/robot/Robot.java
@@ -25,7 +25,7 @@ public class Robot extends IterativeRobot {
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
-    P51Talon fRight, bRight, fLeft, bLeft, lShooter, rShooter, aMotor, smotor;
+    P51Talon fRight, bRight, fLeft, bLeft, lShooter, rShooter, aMotor;
     Shooter myShooter;
     Joystick lJoystick, rJoystick, sJoystick;
     RobotDrive myRobot;
